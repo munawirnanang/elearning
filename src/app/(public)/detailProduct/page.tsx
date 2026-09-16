67637eab4d61d948cc3a27053c1a3ba800bc15c5
Muhamad Munawir Amin <50771898+munawirnanang@users.noreply.github.com>
2026-09-16 11:38:41 +0700

    update struktur folder 2 perubahan

--- src/app/(public)/detailProduct/page.tsx
+++ src/app/(public)/detailProduct/page.tsx
@@ -1,7 +1,7 @@
 import Navigation from '@/components/public/header/header';
 import Footer from '@/components/public/footer/footer';
 
-import DetailProduct from '@/features/public/detailProduct/detail-product';
+import DetailProduct from '@/features/public/detail-product/detail-product';
 
 export default function DetailProductPage() {
   return (
